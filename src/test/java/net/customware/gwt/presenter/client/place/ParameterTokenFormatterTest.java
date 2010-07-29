d2e63bbc1c8be8e1bcaa5c16ed4fad9b5711e143
Robert Munteanu <robert.munteanu@gmail.com>
2010-07-29 10:31:01 +0300

    Added test for rendering of empty parameter.

--- src/test/java/net/customware/gwt/presenter/client/place/ParameterTokenFormatterTest.java
+++ src/test/java/net/customware/gwt/presenter/client/place/ParameterTokenFormatterTest.java
@@ -24,6 +24,12 @@ public class ParameterTokenFormatterTest {
         assertPlaceIsRenderedAs(new PlaceRequest("test"), "test");
     }
 
+    @Test
+    public void emptyParameterValue() {
+    	
+    	assertPlaceIsRenderedAs(new PlaceRequest("test").with("Description", "").with("SomethingElse", "hello"), "test;Description=;SomethingElse=hello");
+    }
+
     private void assertPlaceIsRenderedAs(PlaceRequest placeRequest, String expectedHistoryToken) {
 
         ParameterTokenFormatter formatter = new ParameterTokenFormatter();
