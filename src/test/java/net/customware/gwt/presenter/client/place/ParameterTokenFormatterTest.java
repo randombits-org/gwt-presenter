ab86ffd6a1ab38ebdf754a0e454d9b490382a837
Robert Munteanu <robert.munteanu@gmail.com>
2010-07-29 11:14:26 +0300

    Fixes Issue 22: Place request does not support parameter with an empty value

--- src/test/java/net/customware/gwt/presenter/client/place/ParameterTokenFormatterTest.java
+++ src/test/java/net/customware/gwt/presenter/client/place/ParameterTokenFormatterTest.java
@@ -7,27 +7,53 @@ import org.junit.Test;
 public class ParameterTokenFormatterTest {
 
     @Test
-    public void parameterValue() {
+    public void renderParameterValue() {
 
         assertPlaceIsRenderedAs(new PlaceRequest("test").with("name", "value"), "test;name=value");
     }
 
     @Test
-    public void escapedParameterValue() {
+    public void renderEscapedParameterValue() {
 
         assertPlaceIsRenderedAs(new PlaceRequest("test").with("name", "value;value"), "test;name=value;;value");
     }
 
     @Test
-    public void noParameterValue() {
+    public void renderNoParameterValue() {
         
         assertPlaceIsRenderedAs(new PlaceRequest("test"), "test");
     }
 
     @Test
-    public void emptyParameterValue() {
+    public void renderEmptyParameterValue() {
     	
     	assertPlaceIsRenderedAs(new PlaceRequest("test").with("Description", "").with("SomethingElse", "hello"), "test;Description=;SomethingElse=hello");
+    }
+    
+    @Test
+    public void parseParameterValue() {
+
+    	assertTokenIsParsedAs("test;name=value", new PlaceRequest("test").with("name", "value"));
+    }
+
+    @Test
+    public void parseNoParameterValue() {
+        
+    	assertTokenIsParsedAs("test", new PlaceRequest("test"));
+    }
+    
+    @Test
+    public void parseEmptyParameterValue() {
+    	
+    	assertTokenIsParsedAs("test;Description=;SomethingElse=hello", new PlaceRequest("test").with("Description", "").with("SomethingElse", "hello"));
+    }
+    
+    private void assertTokenIsParsedAs(String historyToken, PlaceRequest expectedPlaceRequest) {
+    	
+    	ParameterTokenFormatter formatter = new ParameterTokenFormatter();
+    	PlaceRequest placeRequest = formatter.toPlaceRequest(historyToken);
+    	
+    	assertEquals("Token improperly parsed", expectedPlaceRequest, placeRequest);
     }
 
     private void assertPlaceIsRenderedAs(PlaceRequest placeRequest, String expectedHistoryToken) {
